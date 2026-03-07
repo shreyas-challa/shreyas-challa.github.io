f035794d66824e33f7953db07d8664cf7d824060
Shreyas Challa <shreyas.challa3@gmail.com>
2026-03-07 18:53:30 -0500

    Center Latest Post heading above featured blog card

--- src/mainheading.jsx
+++ src/mainheading.jsx
@@ -2,15 +2,13 @@ import { TypingAnimation } from "@/components/ui/typing-animation";
 
 export function MainHeading() {
   return (
-    <div >
-      <div>
-        <TypingAnimation
-          words={["Latest Post"]}
-          cursorStyle="block"
-          loop={false}
-          className="text-4xl font-bold"
-        />
-      </div>
+    <div className="w-full max-w-[1200px] mx-auto px-4 md:px-0">
+      <TypingAnimation
+        words={["Latest Post"]}
+        cursorStyle="block"
+        loop={false}
+        className="text-4xl font-bold"
+      />
     </div>
   )
 }
